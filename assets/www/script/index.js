2c6406ac3c9e6405329929f4badf7250c886c47a
Jason Teplitz <jteplitz602@gmail.com>
2011-07-18 17:16:50 -0400

    Finished creating accounts and started working on addresses

--- assets/www/script/index.js
+++ assets/www/script/index.js
@@ -1,3 +1,7 @@
+/**
+ * TODO: fix the way errors are displayed
+ */
+var currEmail, currPass;
 $(window).load(function(){
 	Ordrin.initialize("shds1d6c4BGDGs8", "http://nn2.deasil.com"); // for now this will be deasil
 	 console.log("load");
@@ -26,10 +30,13 @@ $(window).load(function(){
 		});
 	});
 	$("#createAccount_btn").click(function(){
-		//$.mobile.changePage($("#createAccount"), "slidedown", false, true);
 		$("#postAccount_btn").click(function(){
-			Ordrin.u.makeAcct($("#createEmail").val(), $("#createPassword").val(), $("#createFirstName").val(), $("#createLastName").val(), function(data){
+			currEmail = $("#createEmail").val();
+			currPass  = $("#createPassword").val();
+			$.mobile.pageLoading();
+			Ordrin.u.makeAcct(currEmail, currPass, $("#createFirstName").val(), $("#createLastName").val(), function(data){
 				data = eval ("(" + data + ")");
+				$.mobile.pageLoading(true);
 				if (data._error != undefined && data._error != 0){
 					$.mobile.changePage($("#error"), "pop", false, true);
 					$("#errorMsg").html(data.msg); // make this corresspond to the error from the server
@@ -37,9 +44,23 @@ $(window).load(function(){
 						$("#error").dialog("close");
 					});
 				}else{
-					$("#createAccount").dialog('close');
+					$("#createAccount").append("<a href = '#restaurant' id = 'removeMe'></a>");
+					$("#removeMe").click().remove();
+					Ordrin.u.setCurrAcct(currEmail, currPass);
+					getAddresses();
 				}
 			});
 		})
 	});
+	$("#login").load(function(){
+		console.log("login");
+	})
 });
+function getAddresses(){
+	Ordrin.u.getAddress("", function(data){
+		data = eval ("(" + data + ")");
+		if (data == "[]"){ // the user has no addresses so push the craete address dialog
+			
+		}
+	});
+}
