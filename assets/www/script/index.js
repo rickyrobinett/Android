c40f58801879639b4f56ed503dc58a45a88792b5
Jason Teplitz <jteplitz602@gmail.com>
2011-07-19 10:16:26 -0400

    made login functional and it proceeds to the new address scene. Also made new address scene work and pass through to the getRestaurantList function which doesnt exist yet but will soon. If you log in with only one current address it defaults to that address.

--- assets/www/script/index.js
+++ assets/www/script/index.js
@@ -1,26 +1,25 @@
 /**
  * TODO: fix the way errors are displayed
  * 		 encapsulate the code? Something needs to be done to make it less messy. Maybe just move the handlers into their own functions, possible in seperate files
+ * 		 Save user account (use phonegap for local data storage)
+ * 		 Save previous address (use phonegap for local data storage)
  */
 var currEmail, currPass;
 $(window).load(function(){
 	Ordrin.initialize("shds1d6c4BGDGs8", "http://nn2.deasil.com"); // for now this will be deasil
-	 console.log("load");
 	$("body").append("<a href = '#login' id = 'removeMe' data-rel = 'dialog' data-transition = 'pop'></a>");
 	$("#removeMe").click().remove();
+	
 	 $("#login_btn").click(function(){
 		var email = $("#loginEmail").val();
 		var pass  = $("#loginPassword").val();
 		$.mobile.pageLoading();
 		Ordrin.u.setCurrAcct(email, pass);
 		Ordrin.u.getAcct(function(data){
-			data = eval ("(" + data + ")");
+			data = JSON.parse(data);
 			if (data._error != undefined && data._error != 0){
 				$.mobile.changePage($("#error"), "pop", false, true);
 				$("#errorMsg").html(data.msg);
-				$("#errorClose_btn").click(function(){
-					$("#error").dialog("close");
-				});
 			}else{
 				$("#createAccount").append("<a href = '#restaurant' data-rel = 'back' id = 'removeMe'></a>");
 				$("#removeMe").click().remove();
@@ -29,6 +28,7 @@ $(window).load(function(){
 				
 		});
 	});
+	
 	var time = new Date();
 	time.setASAP();
 	var place = new Address("1 Main Street", "", "Weston", "32501")
@@ -39,19 +39,17 @@ $(window).load(function(){
 		$.tmpl("restListTemp", data).appendTo("#restList");
 		$("#restList").listview("refresh");
 	});
+	
 	$("#postAccount_btn").click(function(){
 		currEmail = $("#createEmail").val();
 		currPass  = $("#createPassword").val();
 		$.mobile.pageLoading();
 		Ordrin.u.makeAcct(currEmail, currPass, $("#createFirstName").val(), $("#createLastName").val(), function(data){
-			data = eval ("(" + data + ")");
+			data = JSON.parse(data);
 			$.mobile.pageLoading(true);
 			if (data._error != undefined && data._error != 0){
 				$.mobile.changePage($("#error"), "pop", false, true);
 				$("#errorMsg").html(data.msg); // make this corresspond to the error from the server
-				$("#errorClose_btn").click(function(){
-					$("#error").dialog("close");
-				});
 			}else{
 				$("#createAccount").append("<a href = '#restaurant' id = 'removeMe'></a>");
 				$("#removeMe").click().remove();
@@ -60,7 +58,13 @@ $(window).load(function(){
 			}
 		});
 	});
+	
+	// set the button in the error dialog to always close it (we should be able to override this on an as needed basis)
+	$("#errorClose_btn").click(function(){
+		$("#error").dialog("close");
+	});
 });
+
 function getAddresses(){
 	Ordrin.u.getAddress("", function(data){
 		if (data == "[]"){ // the user has no addresses so push the create address dialog
@@ -70,10 +74,26 @@ function getAddresses(){
 			$("#createAddress_btn").click(function(){
 				var place = new Address($("#createAddressAddress").val(), $("#createAddressAddress2").val(), $("#createAddressCity").val(), $("#createAddressZip").val(), $("#createAddressState").val(), $("#createAddressPhone").val(), $("#createAddressName").val());
 				Ordrin.u.updateAddress(place, function(data){
-					console.log(data);
-				})
+					data = JSON.parse(data);
+					if (data._error != undefined && data._error != 0){
+						$("body").append("<a href = '#error' data-rel = 'dialog' id = 'removeMe'></a>");
+						$("#removeMe").click().remove();
+						$("#errorMsg").html(data.msg);
+					}else{
+						$("#createAddress").append("<a href = '#restaurant' id = 'removeMe'></a>");
+						$("#removeMe").click().remove();
+						getRestaurantList(place, "ASAP");
+					}
+				});
 			});
+			return;
 		}
-		data = eval ("(" + data + ")");
+		data = JSON.parse(data);
+		if (data.length == 1){ // the user only has one address so convert the object and send it straight to the resturant list
+			var place = new Address(data[0].addr, data[0].addr2, data[0].city, data[0].zip, data[0].state, data[0].phone, data[0].nick);
+			getRestaurantlist(place, "ASAP");
+		}else{ // the user has more than 1 address so open a dialog to let them choose which address to use and the get the restaurant list. Possibly save their choice?
+			
+		}
 	});
 }
